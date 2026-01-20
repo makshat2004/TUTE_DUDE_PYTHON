320ded12bcfafce517830734899efa68366f88db
makshat2004 <akshatmamtani7588@gmail.com>
2026-01-20 13:29:25 +0530

    ASSIGNMENT1

---
+++ ASSIGNMENT1/README.md
@@ -0,0 +1,9 @@
+# TASK 1
+
+- This program takes the input of 2 numbers 
+and display the basic operations of addition, multiplication, subtraction, division using those 2 numbers.
+
+
+# TASK 2
+- This program takes the input as first name and the last name
+of the user and concatenates it into the full name of the user and uses that full name to print out a greeting for them.
---
+++ ASSIGNMENT1/Task1.py
@@ -0,0 +1,18 @@
+"""
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+	Addition
+	Subtraction
+	Multiplication
+	Division
+3.  Displays the results of each operation on the screen.
+"""
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition: ", num1 + num2)
+print("Subtraction: ", num1 - num2)
+print("Multiplication: ", num1 * num2)
+print("Division: ", num1 / num2)
